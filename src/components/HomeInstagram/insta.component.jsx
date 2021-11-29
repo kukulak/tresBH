8d7329ffb991a071bc2fef601c90df766d4d6399
Christian Valderrama <kukulak.cero@gmail.com>
2021-11-29 14:24:58 -0600

    cuadricula home

--- src/components/HomeInstagram/insta.component.jsx
+++ src/components/HomeInstagram/insta.component.jsx
@@ -4,9 +4,19 @@ import './insta.styles.scss';
 
 
 const TimeInstagram = ( { id, media_url, num } ) => {
+    let instaImage
+    if(media_url.indexOf('video') === -1){
+        instaImage = <img className={`instaImage grid${num}`} src={media_url} alt={`insta${id}`} />
+    } else {
+        instaImage = null
+        // instaImage = <div className='social3bh'>@3BH</div>
+
+    }
+   
     return(
         <div className={`grid${num}`}>
-            <img className={`instaImage grid${num}`} src={media_url} alt={`insta${id}`}/>
+           {instaImage}
+            {/* <img className={`instaImage grid${num}`} src={media_url} alt={`insta${id}`}/> */}
             {/* <img className="instaImage" src={media_url} alt="insta"/>  */}
         </div>
     )
